0b19c0de2fe55fdfa39d57632c9124410e6be0cb
kevinkkimutai <kiruikevinkim@gmail.com>
2023-03-29 17:39:04 +0300

    books model updated

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,24 +1,3 @@
 class Book < ApplicationRecord
-    belongs_to :user
-
-
-
-    # validates :title, {
-    #   length: { minimum: 5, maximum: 20 },
-    #   presence: true
-    # }
-  
-    # validates :description, {
-    #   length: { minimum: 20 },
-    #   presence: true
-    # }
-  
-    # validates :status, {
-    #   numericality: { in: 0..3 }
-    # }
-  
-    # validates :priority, {
-    #   numericality: { in: 0..2 }
-    # }
-  
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2023_03_28_115158) do
-=======
->>>>>>> 39f59ad679fc1d1ce69b5dbbe9636f87d67941c8
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_093530) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_143203) do
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -27,20 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_093530) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "book_id", null: false
     t.integer "quantity"
-    t.integer "total_price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_orders_on_book_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -53,11 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_093530) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
+  add_foreign_key "books", "users"
   add_foreign_key "orders", "books"
   add_foreign_key "orders", "users"
-=======
-
-  add_foreign_key "books", "users"
->>>>>>> 39f59ad679fc1d1ce69b5dbbe9636f87d67941c8
 end
